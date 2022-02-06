22ff4cde5cec453e5938192a379fbbca43225fe2
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-06 15:31:56 +0700

    Add files via upload

---
+++ .,/MspamX/bin/McybearX.py
@@ -0,0 +1,8 @@
+import os,random,time
+def iklan():
+        iklan_gak=[]
+        kan = iklan_gak.append(random.choice(['y', 'g']))
+        if "y" in iklan_gak:
+                os.system("xdg-open https://cararegistrasi.com/VGWDEihZia")
+        elif "g" in iklan_gak:
+                pass
