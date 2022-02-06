6c40eac8febef70de2ab5c09508746fb04108804
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-06 11:51:53 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+pkg update && pkg upgrade
+pkg install mpv -y
+pkg install play-audio -y
+pkg install python2 -y
+pkg install python -y
+pkg install pip -y
+pkg install git -y
+pkg install curl -y
+
+pip install requests -y
+pip install bs4 -y
+pip install futures -y
+pip install mechanize -y
+pip install ipaddress -y
+pip install hashlib -y
