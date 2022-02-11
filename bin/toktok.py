3ee1b1ea816346fdd0df518609b43aabe671aea8
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-11 17:31:23 +0700

    Delete toktok.py

--- bin/toktok.py
+++
@@ -1,6 +0,0 @@
-import os
-tok1="vavhwhoaiavavwjhavvUWVWGhgUGHGHggguvGGGGGvcfJkvdrOwkbwkabV1881jajowoqbbabbsbwghwkqygwvvwkaoo++#))#!*;+++=^`"
-tok2="hhwvvwvwhwjhbwJjJBJFDtUkkKkBvacafJKbVVhhvVvgjhhv:::'cdd&6°{{{√√`j😇😇😊😀😍😊😁😀😝😘😌😋😋😇😇🥸😣😣😣😣😕"
-tok3="hvabqkqkkjJBvcfquqfrughrd🙂😛😀😁😁😁😍😃😘😏😏😒😒🥳🤡🤡🤡🤡😚🥸🤗🤗😚😛😉😏☹️🥳🤭😴🤮✌🤚✋🤘😿👂👂👄👂👄👂"
-tok4="HHhGFFjqkkaklalaojbvwvbbbgYUFYTYHtydfsjwkkjj+7272528+#;#;#(((;--6vhhygqqkkKkjJJHHGG°°^`{`×`{{~[[~℅hhhwkwkwn"
-tok5="jEEAAASBBDBSNKWKLNNNNNNNNNNwkkwwpwplwbabbb81816JJJBABBWBnjnjskwvwvlqlnabbaavvshwkwkn;:;$(#!hhsvakwlwknabaka"
