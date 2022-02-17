9995b32a2dbc9e73c6f5bbd3f5f79306fb0db101
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-17 17:20:17 +0700

    Delete McybearX.py

--- bin/McybearX.py
+++
@@ -1,9 +0,0 @@
-import os,random,time
-def iklan():
-        iklan_gak=[]
-        kan = iklan_gak.append(random.choice(['y', 'g']))
-        if "y" in iklan_gak:
-                pass
-#                os.system("xdg-open https://cararegistrasi.com/VGWDEihZia")
-        elif "g" in iklan_gak:
-                pass
