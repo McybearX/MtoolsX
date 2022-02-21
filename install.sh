5ba7286813d6f0262b6522ae063ec58cdf5e8c97
Mcybear× <95560890+McybearX@users.noreply.github.com>
2022-02-21 14:57:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ pkg update && pkg upgrade
 pkg install mpv -y
 pkg install play-audio -y
 pkg install python2 -y
+pkg install python3 -y
 pkg install python -y
 pkg install pip -y
 pkg install git -y
